9a2612eb9a9b193f669a7c0d72cd29aaea2f1de6
SOMAPAZ <167923562+SOMAPAZ@users.noreply.github.com>
2024-11-07 04:51:11 -0600

    Update MA_updates.sql

--- db/MA_updates.sql
+++ db/MA_updates.sql
@@ -1,9 +1,7 @@
-ALTER TABLE billing
-  DROP id_amountwater;
-ALTER TABLE billing ADD water DECIMAL(10,2) NOT NULL AFTER month;
-ALTER TABLE `billing`
-  DROP `id_sanitation`;
+ALTER TABLE `billing` DROP `id_amountwater`;
+ALTER TABLE `billing` ADD `water` DECIMAL(10,2) NOT NULL AFTER`month`;
+ALTER TABLE `billing` DROP `id_sanitation`;
 ALTER TABLE `billing` ADD `sanitation` DECIMAL(10,2) NOT NULL AFTER `drain_iva`;
-ALTER TABLE `billing` CHANGE `year` `year` INT(10) NOT NULL;
+ALTER TABLE `billing` CHANGE `year` `year` INT NOT NULL;
 ALTER TABLE `billing` CHANGE `date_billing` `date_billing` VARCHAR(16) NOT NULL;
-ALTER TABLE `billing` CHANGE `id_employment` `id_employment` BIGINT(20) NULL;
+ALTER TABLE `billing` CHANGE `id_employment` `id_employment` BIGINT NULL;
