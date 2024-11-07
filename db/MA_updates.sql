f26e1b71e0eb496753e3bce2f92ab91f87e0b9a0
somap24 <somapaz2024@gmail.com>
2024-11-07 03:58:26 -0600

    Update MA_updates.sql

--- db/MA_updates.sql
+++ db/MA_updates.sql
@@ -1,3 +1,6 @@
 ALTER TABLE billing
   DROP id_amountwater;
 ALTER TABLE billing ADD water DECIMAL(10,2) NOT NULL AFTER month;
+ALTER TABLE `billing`
+  DROP `id_sanitation`;
+ALTER TABLE `billing` ADD `sanitation` DECIMAL(10,2) NOT NULL AFTER `drain_iva`;
