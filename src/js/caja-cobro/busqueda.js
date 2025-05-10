1ed7595e5cfe83912173d1329ec54d66f2699717
Carlos Cabrera <deming3c@outlook.com>
2025-05-09 18:53:29 -0600

    buscador caja

--- src/js/caja-cobro/busqueda.js
+++ src/js/caja-cobro/busqueda.js
@@ -1,49 +1,104 @@
 import { limpiarHTML } from "../helpers/index.js";
 import GetDatos from "../classes/GetData.js"
+import Modal from "../classes/Modal.js";
 
 (() => {  
-  const inputBusqueda = document.querySelector("#busqueda");
-  const listadoCoincidencias = document.querySelector("#listado-coincidencias ul");
-  let coincidencias = [];
-  
-  let usuarios = [];
+  const btnConsultar = document.querySelector("#btn-search-user");
 
-  document.addEventListener("DOMContentLoaded", () => {
+  if(btnConsultar){
+    const ancoreConsulta = document.querySelector("#a-search-user");
+    btnConsultar.addEventListener("click", modal);
+    ancoreConsulta.addEventListener("click", modal);
+    let usuarios = [];
+    let usuariosFiltrados = [];
+
     obtenerUsuarios();
-    inputBusqueda.addEventListener("input", buscarUsuario);
-  });
+    async function obtenerUsuarios() {
+      const usuariosFetch = await GetDatos.consultar(`${location.origin}/api/users`);
+      formatearUsuarios(usuariosFetch);
+    }
 
-  const obtenerUsuarios = async () => usuarios = await GetDatos.consultar(`${location.origin}/api/users`);
+    function modal() {
+      const formulario = document.createElement('FORM');
+      formulario.className = 'w-full mx-auto my-5';
+      formulario.setAttribute('autocomplete', 'off');
+      formulario.onsubmit = buscarDeuda
+      const label = document.createElement('LABEL');
+      label.className = 'block text-sm font-bold text-gray-700 dark:text-gray-200 uppercase mb-3';
+      label.textContent = 'Nombre, Dirección o ID del usuario: (5 caracteres mínimo)';
+      label.setAttribute('for', 'busqueda');
+      const inputBusqueda = document.createElement('INPUT');
+      inputBusqueda.setAttribute('id', 'busqueda');
+      inputBusqueda.className = 'w-full px-4 py-2 bg-gray-200 rounded-lg border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white outline-none';
+      inputBusqueda.setAttribute('placeholder', 'Nombre, Dirección o ID. Ej. 1234 - Juan Pérez');
+      inputBusqueda.oninput = obtenerValorInput;
+      
+      const listadoCoincidencias = document.createElement('DIV');
+      listadoCoincidencias.className = "my-2";
+      listadoCoincidencias.setAttribute('id', 'listado-coincidencias');
+      const listadoCoincidenciasUl = document.createElement('UL');
+      listadoCoincidenciasUl.className = "space-y-2";
+      listadoCoincidencias.appendChild(listadoCoincidenciasUl);
 
-  const buscarUsuario = () => {
-    limpiarHTML(listadoCoincidencias);
-    const valor = inputBusqueda.value.trim();
+      const btnBuscar = document.createElement('BUTTON');
+      btnBuscar.className = 'text-sm font-bold px-4 py-2 bg-indigo-600 rounded text-white hover:bg-indigo-700';
+      btnBuscar.textContent = 'Consultar';
+      btnBuscar.setAttribute('type', 'submit');
 
-    if(valor.length > 2) {
-      coincidencias = [...usuarios].filter((usuario) => {
-        const con = `${usuario.id} - ${usuario.nombre} - ${usuario.direccion}`;
-        return con.toLowerCase().includes(valor.toLowerCase());
-      });
+      formulario.appendChild(label);
+      formulario.appendChild(inputBusqueda);
+      formulario.appendChild(listadoCoincidencias);
+      Modal.renderModal(formulario, btnBuscar);
+    }
 
-      limpiarHTML(document.querySelector("#datos-usuario"));
-      limpiarHTML(document.querySelector("#deuda-usuario"));
-      limpiarHTML(document.querySelector("#boton"));
+    function obtenerValorInput(e) {
+      const value = e.target.value.trim();
 
-      coincidencias.map((coincidencia) => {
-        const li = document.createElement("LI");
-        li.className =
-          "block bg-white shadow rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700 dark:bg-gray-800 dark:text-gray-200 uppercase cursor-pointer";
+      if(value.length > 5) {
+        const expresion = new RegExp(value, 'i');
+        usuariosFiltrados = usuarios.filter( usuario => {
+            if(usuario.data.search(expresion) != -1){
+                return usuario
+            }
+        });
+      } else {
+        usuariosFiltrados = [];
+      }
+
+      renderList(usuariosFiltrados);
+    }
+
+    function formatearUsuarios(usuariosFetch) {
+      usuarios = usuariosFetch.map(usuario => {
+        return{
+          data: `${usuario.nombre.trim()} - ${usuario.direccion.trim()}`, 
+          id: usuario.id
+        }
+      })
+    }
+
+    function renderList(usuariosFiltrados) {
+      const listadoCoincidencias = document.querySelector("#listado-coincidencias ul");
+      limpiarHTML(listadoCoincidencias);
+      usuariosFiltrados.forEach(usuario => {
+        const li = document.createElement("li");
+        li.className = "block shadow rounded-lg bg-gray-100 hover:bg-gray-200 px-4 py-2 text-sm font-medium text-gray-700 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-gray-200 uppercase cursor-pointer";
   
-        li.textContent = `${coincidencia.id} - ${coincidencia.nombre} - ${coincidencia.direccion}`;
-        li.onclick = () => autocompletar(coincidencia);
+        li.textContent = `${usuario.data}`;
+        li.onclick = () => autocompletar(usuario);
         listadoCoincidencias.appendChild(li);
       });
     }
-    
-  }
 
-  const autocompletar = (usuario) => {
-    inputBusqueda.value = `${usuario.id.trim()} - ${usuario.nombre.trim()}`;
-    limpiarHTML(listadoCoincidencias);
+    const autocompletar = (usuario) => {
+      const inputBusqueda = document.querySelector("#busqueda");
+      inputBusqueda.value = `${usuario.id} - ${usuario.data}`;
+      limpiarHTML(document.querySelector("#listado-coincidencias ul"));
+    }
+
+    function buscarDeuda(e) {
+      e.preventDefault();
+      console.log(e.target)
+    }
   }
 })();
--- views/caja/index.php
+++ views/caja/index.php
@@ -3,24 +3,24 @@
     <article class="mx-auto max-w-screen-lg px-4 2xl:px-0">
         <div class="lg:flex lg:flex-col lg:items-center lg:gap-4">
             <h2 class="shrink-0 font-black text-xl text-gray-900 dark:text-white sm:text-4xl uppercase text-center">
-                Consultar Usuario
+                Consulta del Adeudo del Usuario
             </h2>
 
-            <form class="w-full mx-auto mt-5" id="formulario-busqueda" autocomplete="off">
-                <div id="error"></div>
-                <label for="busqueda" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                <div class="relative">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
-                    </div>
-                    <input type="search" id="busqueda" class="block w-full  bg-white p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white uppercase" placeholder="Id o Nombre del usuario" />
-                    <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-indigo-700 hover:bg-indigo-800 font-medium rounded text-sm px-4 py-2 dark:bg-indigo-600 dark:hover:bg-indigo-700">Buscar</button>
-                </div>
-            </form>
-            <div id="listado-coincidencias" class="my-2">
-                <ul class="space-y-2"></ul>
+            <div class="my-10 flex flex-col items-center justify-center gap-6">
+                <p class="text-lg text-center dark:text-white">
+                    No hay ninguna consulta previa,
+                    <a id="a-search-user" class="text-indigo-600 hover:underline font-bold cursor-pointer">agrega una consulta para comenzar</a>
+                </p>
+
+                <button
+                    id="btn-search-user"
+                    class="text-sm font-bold uppercase px-6 py-2 bg-indigo-600 rounded text-white hover:bg-indigo-700">
+                    Buscar
+                </button>
+            </div>
+
+            <div>
+                <img src="build/img/bg-search-debt.svg" alt="buscar usuario" class="h-96">
             </div>
         </div>
     </article>
