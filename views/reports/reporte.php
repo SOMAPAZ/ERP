c67d332e46be700d9c9ef9edc781659ce7c7556e
Carlos Cabrera <deming3c@outlook.com>
2025-05-21 14:27:03 -0600

    Agregado btn reportes cobro extra

--- views/reports/reporte.php
+++ views/reports/reporte.php
@@ -123,6 +123,14 @@
             </div>
         </div>
         <div class="flex flex-col md:flex-row items-center justify-end gap-2">
+            <?php if ($reporte->id_user): ?>
+                <a href="/crear-nota_reporte?folio=<?= $reporte->id ?>" class="w-full md:w-auto flex items-center rounded border border-gray-200 bg-white px-4 py-2 hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" id="btn-add-note">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 me-2 dark:text-white">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <span class="text-sm font-medium text-gray-900 dark:text-white">Costo a usuario</span>
+                </a>
+            <?php endif; ?>
             <a href="/crear-nota_reporte?folio=<?= $reporte->id ?>" class="w-full md:w-auto flex items-center rounded border border-gray-200 bg-white px-4 py-2 hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" id="btn-add-note">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 me-2 dark:text-white">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
